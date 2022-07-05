4b0f01eb7f817f07c2af776057e4e9b6062c91d6
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-07-05 11:27:39 -0700

    Update __init__.py

--- battlefield/__init__.py
+++ battlefield/__init__.py
@@ -1,5 +1,5 @@
-from arcade import SpriteList, Window, draw_point
-from arcade import run
+from arcade import SpriteList, Window
+from arcade import draw_point, enable_timings, get_fps, run
 
 import os
 import sys
@@ -9,26 +9,31 @@ parent = os.path.dirname(current)
   
 sys.path.append(parent)
 
-from sprite import Soldier, Arrow
 from constants import *
 from color import RED, WHITE
+from widgets import Container, Label
 
 from units import Unit
-from variables import enemy_list, player_list
+from variables import Arrow, Soldier
 
+# enable_timings()
 
 class Battlefield(Window):
 
     def __init__(self):
         Window.__init__(self, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
-        global player_list, enemy_list
+        self.player_list = SpriteList()
+        self.enemy_list = SpriteList()
+        self.projectile_list = SpriteList()
         
-        self.player_list = player_list
-        self.enemy_list = enemy_list
-        
-        self.unit = Unit(player_formation, player_list, 100, 50)
-        
+        self.player_unit = Unit(player_formation, PLAYER, 200, 250)
+        self.enemy_unit = Unit(enemy_formation, ENEMY, 400, 800)
+
+        self.container = Container()
+        self.label = Label("", 50, 50)
+
+        self.container.append(self.label)
 
         self.background_color = WHITE
     
@@ -37,13 +42,19 @@ class Battlefield(Window):
 
         self.player_list.draw()
         self.enemy_list.draw()
+        self.projectile_list.draw()
 
-        self.player_list = player_list
-        self.enemy_list = enemy_list
+        self.label.text = f"{int(get_fps())} fps"
 
+        self.container.draw()
+        
     def on_update(self, delta_time):
         self.player_list.update()
         self.enemy_list.update()
+        self.projectile_list.update()
+
+        # import arcade.examples.performance_statistics
+
 
 if __name__ == "__main__":
     battlefield = Battlefield()
