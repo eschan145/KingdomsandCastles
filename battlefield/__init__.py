1ca0d521b318c35be702fa3d77df26f70f984061
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-07-06 10:53:58 -0700

    Update __init__.py

--- battlefield/__init__.py
+++ battlefield/__init__.py
@@ -1,4 +1,4 @@
-from arcade import SpriteList, Window
+from arcade import SpriteList, Window, close_window, draw_rectangle_filled, draw_rectangle_outline
 from arcade import draw_point, enable_timings, get_fps, run
 
 import os
@@ -11,32 +11,55 @@ sys.path.append(parent)
 
 from constants import *
 from color import RED, WHITE
-from widgets import Container, Label
+from widgets import Container, Frame, Label, Toggle
 
 from units import Unit
 from variables import Arrow, Soldier
+from key import Q, W
 
 # enable_timings()
 
 class Battlefield(Window):
 
     def __init__(self):
-        Window.__init__(self, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        Window.__init__(self, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, resizable=True)
 
         self.player_list = SpriteList()
         self.enemy_list = SpriteList()
         self.projectile_list = SpriteList()
+
+        self.units = []
         
-        self.player_unit = Unit(player_formation, PLAYER, 200, 250)
-        self.enemy_unit = Unit(enemy_formation, ENEMY, 400, 800)
+        self.player_unit = Unit(player_formation, PLAYER, self.width / 2, 200)
+        self.enemy_unit = Unit(enemy_formation, ENEMY, self.width / 2, 500)
+
+        self.current_unit = self.player_unit
 
         self.container = Container()
-        self.label = Label("", 50, 50)
 
-        self.container.append(self.label)
+        self.fps = Label("", 50, 50, command=close_window)
+
+        self.unit_frame = Frame(self.width - UNIT_FRAME_WIDTH / 2, self.height,
+                                UNIT_FRAME_WIDTH, UNIT_FRAME_HEIGHT, TOP)
+        self.unit_frame.top = 0
+        
+        self.unit_move_forward = Toggle("Move forwards", 10, 20)
+        self.unit_organize_volley = Label("Organize volley", 10, 40, self.unit_frame,
+                                          command=self.command, parameters=["volley"])
+        
+
+        self.container.append(self.fps)
+        self.container.append(self.unit_move_forward)
+        self.container.append(self.unit_organize_volley)
+
+        self.unit_organize_volley.bind(Q)
 
         self.background_color = WHITE
     
+    def command(self, attack):
+        if attack == "volley":
+            self.current_unit.on_volley()
+
     def on_draw(self):
         self.clear()
 
@@ -44,16 +67,21 @@ class Battlefield(Window):
         self.enemy_list.draw()
         self.projectile_list.draw()
 
-        self.label.text = f"{int(get_fps())} fps"
-
-        self.container.draw()
+        self.fps.text = f"{int(get_fps())} fps"
         
+        self.container.draw()
+
+        for unit in self.units:
+            unit.draw()
+    
+    def on_key_press(self, keys, modifiers):
+        if keys == W:
+            self.current_unit.on_split()
+
     def on_update(self, delta_time):
         self.player_list.update()
         self.enemy_list.update()
         self.projectile_list.update()
-
-        # import arcade.examples.performance_statistics
 
 
 if __name__ == "__main__":
