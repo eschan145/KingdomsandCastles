3c767a84182e3ba7cfb4d54a63ca9b62b47d00c2
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-07-26 13:30:37 -0700

    Added documentation to geometric functions

--- geometry.py
+++ geometry.py
@@ -1,25 +1,134 @@
+"""Define some geometric functions for Armies."""
+
+from cmath import cos, sin
+from random import randrange
 from arcade import Sprite, SpriteList
-from math import hypot
+from math import atan2, degrees, hypot, radians
 from typing import cast, List
 
+points = 0 # Number of points to create unique keysets
+pi = 3.14159265358979
+
+__all__ = [
+           "Point",
+           "square",
+           "cube",
+           "chance",
+           "are_polygons_intersecting",
+           "is_point_in_polygon",
+           "check_collision",
+           "get_distance",
+           "get_closest",
+           "rotate_point",
+           "get_angle_degrees",
+           "get_angle_radians",
+           "degrees_to_radians",
+           "convert_xywh_to_points",
+           "points",
+           "pi",
+           "_check_collision"
+          ]
+
+
 class Point:
+    """Initialize a named Point. This is used in almost every x and y
+    coordinate in Armies."""
     
-    def __init__(self, x=0, y=0):
+    def __init__(self, x, y, name=False):
+        """Initialize a named Point.
+        
+        x - x coordinate of Point
+        y - y coordinate of Point
+        name - unique name of Point (identifier). This is automatically
+               generated with the number of Points created if not specified
+               by this parameter.
+
+        parameters: int, int, str
+
+        properties:
+            x - x coordinate of Point
+            y - y coordinate of Point
+            name - unique keyname of Point
+            data - map of properties
+        
+        methods:
+            __getitem__ - Return a value from key item of data
+        """
+
         self.x = x
         self.y = y
 
-        self.data = {}
+        if not name:
+            # Generate a unique keyset name for this point
+            global points
+            points += 1
 
-    def __getitem__(self, data):
-        return self.data.get(data, False)
+            self.name = str(points)
+            
+        self.data = {
+            "x" : self.x,
+            "y" : self.y,
+            "name" : self.name
+        }
+
+    def __getitem__(self, item):
+        """Return a value from key item of data.
         
+        item - key to find value
+        
+        parameters: str
+        returns: str
+        """
+
+        return self.data.get(item, False)
+        
+
 def square(value):
+    """Calculate the squared value of a number.
+    
+    value - value to take to the power of two
+    
+    parameters: int
+    returns: int
+    """
+
     return pow(value, 2)
 
 def cube(value):
+    """Calculate the cubed value of a number.
+    
+    value - value to take to the power of three
+
+    parameters: int
+    returns: int
+    """
+
     return pow(value, 3)
 
+def chance(value):
+    """Return True or False in a 1-in-value chance.
+    
+    value - chance of returning True
+    
+    parameters: int
+    returns: bool
+    """
+
+    if randrange(1, value + 1) == 2:
+        return True
+    else:
+        return False
+
 def are_polygons_intersecting(a, b):
+    """Check if two polygons are intersecting.
+    
+    a - first polygon bounding box of intersection check
+    b - second polygon bounding box of intersection check
+
+    parameters: int, int
+    returns: bool (True or False if polygons intersecting)
+    """
+
     for polygon in (a, b):
         for i1 in range(len(polygon)):
             i2 = (i1 + 1) % len(polygon)
@@ -53,23 +162,32 @@ def are_polygons_intersecting(a, b):
 
     return True
 
-def is_point_in_polygon(x, y, points):
-    length = len(points)
+def is_point_in_polygon(point, polygon):
+    """See if the given Point exists in a polygon.
+    
+    point - Point to check if in polygon
+    polygon - polygon to recieve test
+
+    parameters: int, int
+    returns: bool (True or False if Point exists in polygon)
+    """
+
+    length = len(polygon)
     inside = False
-    p1x, p1y = points[0]
+    p1x, p1y = polygon[0]
     
     if not length:
         return False
     
     for i in range(length + 1):
         p2x, p2y = points[i % length]
-        if y > min(p1y, p2y):
-            if y <= max(p1y, p2y):
-                if x <= max(p1x, p2x):
+        if point.y > min(p1y, p2y):
+            if point.y <= max(p1y, p2y):
+                if point.x <= max(p1x, p2x):
                     if p1y != p2y:
-                        xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                        xints = (point.y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
                     # noinspection PyUnboundLocalVariable
-                    if p1x == p2x or x <= xints:
+                    if p1x == p2x or point.x <= xints:
                         inside = not inside
                         
         p1x, p1y = p2x, p2y
@@ -77,6 +195,23 @@ def is_point_in_polygon(x, y, points):
     return inside
 
 def _check_collision(a, b):
+    """Internal function for checking collision of two objects. Used by
+    check_collision.
+    
+    a - first object to check collision
+    b - second object to check collision
+    
+    NOTE: you should never need to call this directly.
+    NOTE: you must use an Object or a PhysicsObject. If you want to check
+          collisions with other objects, use are_polygons_interecting with
+          each object's hitbox. Note there are exceptions to these conditions.
+    
+    parameters:
+        a - Object or PhysicsObject
+        b - Object or PhysicsObject
+    returns: list (list of collisions)
+    """
+
     collision_radius = a.collision_radius + b.collision_radius
 
     diff_x = a.position[0] - b.position[0]
@@ -103,6 +238,18 @@ def _check_collision(a, b):
     return intersection
 
 def check_collision(a, b):
+    """Check for collisions between two things. Multiple datatypes are
+    supported. You may use a Object or PhysicsObject, a SpriteList, or a List as parameters and it will calculate the collisions for you.
+    
+    a - first item to check collision with
+    b - second item to check collision with
+    
+    parameters: 
+        a - Object or PhysicsObject,
+        b - PhysicsObject or SpriteList or List
+    returns: list (list of collisions)
+    """
+    
     if isinstance(b, Sprite):
         return _check_collision(a, b)
 
@@ -124,9 +271,29 @@ def check_collision(a, b):
         return sprites
 
 def get_distance(a, b):
+    """Get the distance between two Points.
+    
+    a - first point to get distance
+    b - second point to get distance
+    
+    parameters: Point, Point
+    returns: int - (distance between two points
+    """
+
     return hypot(a.x - b.x, a.y - b.y)
 
 def get_closest(object, list):
+    """Get the closest object from a list to another object.
+    
+    object - object to get distance
+    list - list to get closest object
+    
+    parameters:
+        object - Object or PhysicsObject
+        list - List or SpriteList
+    returns: tuple ((closest, distance))
+    """
+
     if not list:
         return [object, 0]
 
@@ -141,6 +308,100 @@ def get_closest(object, list):
             distance = _distance
 
     return list[position], distance
+
+def rotate_point(point, center, degrees):
+    """Rotate a Point a certain degrees around a center.
+    
+    point - Point to rotate around center
+    center - center the Point rotates around
+    degrees - angle to rotate
+    
+    parameters: Point, Point, int
+    returns: Point
+    """
+
+    temp_x = point.x - center.x
+    temp_y = point.y - center.y
+
+    # now apply rotation
+    radians_ = radians(degrees)
+    cos_angle = cos(radians_)
+    sin_angle = sin(radians_)
+    rotated_x = temp_x * cos_angle - temp_y * sin_angle
+    rotated_y = temp_x * sin_angle + temp_y * cos_angle
+
+    # translate back
+    rounding_precision = 2
+    x = round(rotated_x + center.x, rounding_precision)
+    y = round(rotated_y + center.y, rounding_precision)
+
+    return Point(x, y)
+
+def get_angle_degrees(a, b):
+    """Get angle degrees between two Points.
+    
+    a - first Point to get angle degrees
+    b - second Point to get angle degrees
+    
+    parameters: Point, Point
+    returns: int
+    """
+
+    x_diff = b.x - a.x
+    y_diff = b.y - a.y
+    
+    angle = degrees(atan2(x_diff, y_diff))
+
+    return angle
+
+
+def get_angle_radians(a, b):
+    """Get angle radians between two Points.
+    
+    a - first Point to get angle radians
+    b - second Point to get angle radians
+
+    parameters: Point, Point
+    returns: int
+    """
+
+    x_diff = b.x - a.x
+    y_diff = b.y - a.y
+    angle = atan2(x_diff, y_diff)
+    return angle
+
+def degrees_to_radians(degrees, digits=2):
+    """Convert degrees to radians.
+    
+    degrees - degrees to be converted to radians
+    digits - number of digits of the π value
+    
+    parameters: int
+    returns: int
+    """
+
+    return degrees * round(pi, digits) / 180
+
+def convert_xywh_to_points(point, width, height):
+    """Convert an rectangle with center points and dimensions to only points.
+    
+    point - center Point of rectangle
+    width - width of rectangle
+    height - height of rectangle
+    
+    parameters: Point, int, int
+    returns: tuple (x1, y1, x2, y2)
+    """
+
+    # Note this does not return two Points
+    return (
+        point.x - width / 2,
+        point.y + height / 2,
+        point.x + width / 2,
+        point.y - height / 2
+    )
+
+### DECEPRATED FUNCTIONS ###
 
 def set_hitbox(object):
     height = object.height
@@ -173,4 +434,3 @@ def convert_four_to_one_quadrants(x, y, width, height):
     y = height / 2 + y
 
     return x, y
-    
