2ceb6996292357cdf2a8ec91a0752140033eda5d
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-07-27 14:56:55 -0700

    Updated constants for compatibility with physics

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+"""Contains constants used for Armies"""
+
 # Sides
 
 LEFT = "left"
@@ -24,12 +26,25 @@ TOGGLE_FADE = 17 # How fast the toggle fades during transitions
 
 SLIDER_VELOCITY = 10 # How fast does the slider glide
 
+HORIZONTAL = "horizontal"
+VERTICAL = "vertical"
+
 DEFAULT_FONT_FAMILY = "Montserrat"
 DEFAULT_FONT_SIZE = 12
 
 DEFAULT_FONT = ["Montserrat", 12]
 
+UNIT_FRAME_WIDTH = 700
+UNIT_FRAME_HEIGHT = 200
+
+red_filter = (0b100, 0b110)
+blue_filter = (0b010, 0b110)
+arrow_filter = (0b001, 0b001)
+
 PLAYER = "player"
+ENEMY = "enemy"
+SOLDIER = "soldier"
+
 SQUAD = 12
 SWORD = "sword"
 
@@ -39,22 +54,43 @@ MELEE_RANGE_CHANCE = 10
 RANGE = "range"
 MELEE = "melee"
 
-ARROW_ACCURACY = 20
-ARROW_MAXIMUM_SPEED = 5
-ARROW_ACCELERATION = 0.02
+ARROW = "arrow"
+ARROW_ACCURACY = 10
+ARROW_MAXIMUM_SPEED = 1000
+ARROW_MAXIMUM_ARCHER_SPEED = 1200
+ARROW_SPEED_LOSS = 20
+ARROW_DAMAGE = 20
+ARROW_KNOCKBACK = 3
+ARROW_MOVE_FORCE = 25000
 
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1300
+WINDOW_HEIGHT = 900
 WINDOW_TITLE = "Armies"
 
-SOLDIER_SPACING = 20
+SOLDIER_SPACING = 10
 
 player_formation = [
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+
+    
 ]
 
+enemy_formation = [
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+]
 
 fps_list = [
 
